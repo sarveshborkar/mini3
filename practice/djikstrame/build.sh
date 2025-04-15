719abeb11e058c0d77b6096e7f4d0b23b0171f56
Sarvesh <sarveshborkar25@gmail.com>
2025-04-15 17:48:55 +0530

    inital commit

---
+++ practice/djikstrame/build.sh
@@ -0,0 +1,11 @@
+//
+//  Untitled.h
+//  shared-memory
+//
+//  Created by Sarvesh Borkar on 4/15/25.
+//
+// Assuming user is in working directory djikstrame
+cd build
+cmake ..
+make
+./
---
+++ practice/djikstrame/run.sh
@@ -0,0 +1,8 @@
+#
+//  run.sh
+//  shared-memory
+//
+//  Created by Sarvesh Borkar on 4/15/25.
+//
+// Run build.sh before executing this script.
+./build/djikstrame
