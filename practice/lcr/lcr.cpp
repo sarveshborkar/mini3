5bdd0fafea1c22e9910478fd0b79d7e7c09ef308
Sarvesh Borkar <sarvesh.borkar@sjsu.edu>
2025-04-20 19:42:34 +0530

    finished lcr

--- practice/lcr/lcr.cpp
+++ practice/lcr/lcr.cpp
@@ -2,113 +2,118 @@
 #include <vector>
 #include <iostream>
 
-// TODO : Broken as hell but almost there!
+enum Status { UNKNOWN, CHOSEN, REPORTED };
 
-enum Status {UNKNOWN , CHOSEN, REPORTED};
+struct Message {
+    int uid;
+    bool is_report;
+};
 
-class State{
-
+class State {
 private:
     int uid;
-    std::queue<int> send_queue;
+    std::queue<Message> send_queue;
     Status status;
 
 public:
     State(int id) : uid(id), status(UNKNOWN) {}
 
-    void send(std::vector<State>& ring, int i){
-        if(!send_queue.empty()){
-            int message = send_queue.front();
+    void send(std::vector<State>& ring, int i) {
+        if (!send_queue.empty()) {
+            Message msg = send_queue.front();
             send_queue.pop();
-            int j = (i+1) % ring.size();
-            ring[j].push_to_queue(message);
+            int j = (i + 1) % ring.size();
+            ring[j].push_to_queue(msg);
         }
     }
 
-    void receive(std::vector<State>& ring, int i){
-        if(!send_queue.empty()){
-            int message = send_queue.front();
-            if (status == UNKNOWN){
-                if(message > uid){
+    void receive(std::vector<State>& ring, int i) {
+        if (send_queue.empty()) return;
+
+        Message msg = send_queue.front();
+
+        if (!msg.is_report) {
+            // Electioneering
+            if (status == UNKNOWN) {
+                if (msg.uid > uid) {
                     send_queue.pop();
-                    int j = (i+1) % ring.size();
-                    ring[j].push_to_queue(message);
-                }
-                else if(message == uid){
+                    int j = (i + 1) % ring.size();
+                    ring[j].push_to_queue(msg);
+                } else if (msg.uid == uid) {
+                    send_queue.pop();
                     status = CHOSEN;
-                    send_queue.pop();
-                    std::cout << "CHOSEN:" << uid << std::endl;
+                    std::cout << "CHOSEN: " << uid << std::endl;
+                    Message report = { uid, true };
+                    int j = (i + 1) % ring.size();
+                    ring[j].push_to_queue(report);
+                } else {
+                    send_queue.pop(); 
                 }
-                else{
-                    send_queue.pop();
-                }
+            } else {
+                send_queue.pop();
             }
-            else if (status == CHOSEN || status == REPORTED){
-                if (message == uid && status == CHOSEN){
-                    send_queue.pop();
-                }
-                else{
-                    send_queue.pop();
-                    if (status == UNKNOWN){
-                        status = REPORTED;
-                    }
-                    int j = (i+1) % ring.size();
-                    ring[j].send_queue.push(message);
-                }
-                std::cout << status << ":" << std::endl;
+        } else {
+            // Reporting
+            send_queue.pop();
+            if (msg.uid == uid && status == CHOSEN) {
+                return;
             }
+            if (status == UNKNOWN) {
+                status = REPORTED;
+                std::cout << "REPORTED: " << uid << " (Leader: " << msg.uid << ")" << std::endl;
+            }
+            int j = (i + 1) % ring.size();
+            ring[j].push_to_queue(msg);
         }
-        
     }
 
-    void set_status(Status& nextStatus){
-        status = nextStatus;
-    }
-
-    Status get_status(){
-        return status;
-    }
-
-    int get_uid(){
-        return uid;
-    }
-
-    void push_to_queue(int message){
+    void push_to_queue(Message message) {
         send_queue.push(message);
     }
 
+    Status get_status() {
+        return status;
+    }
+
+    int get_uid() {
+        return uid;
+    }
 };
 
-bool isLeaderElected(std::vector<State>& ring){
-    for (State& state : ring){
-        if(state.get_status() != CHOSEN && state.get_status() != REPORTED){
+bool isLeaderElected(std::vector<State>& ring) {
+    for (State& state : ring) {
+        if (state.get_status() != CHOSEN && state.get_status() != REPORTED) {
             return false;
         }
     }
     return true;
 }
 
-int main(int argc, char* argv[]){
-    std::vector<int> ids = {3, 1 ,4 ,2};
+int main() {
+    std::vector<int> ids = {2, 7 , 1, 8, 3}; 
     std::vector<State> ring;
 
-    for(int id : ids){
-        State state(id);
-        ring.push_back(state);
+    for (int id : ids) {
+        ring.emplace_back(id);
     }
 
-    for (State& state : ring){
-        state.push_to_queue(state.get_uid());
+    for (State& state : ring) {
+        // Setting initial queue with uid and message
+        state.push_to_queue({ state.get_uid(), false });  
     }
 
-    while(!isLeaderElected(ring)){
-        for(int i = 0; i < ring.size(); ++i){
-            ring[i].send(ring,i);
+    int iteration = 1;
+    while (!isLeaderElected(ring)) {
+        std::cout << "Iteration: " << iteration++ << std::endl;
+
+        for (int i = 0; i < ring.size(); i++) {
+            ring[i].send(ring, i);
         }
-        for(int i = 0;i < ring.size(); ++i){
+        for (int i = 0; i < ring.size(); i++) {
             ring[i].receive(ring, i);
         }
     }
 
+    std::cout << "LCR complete." << std::endl;
     return 0;
 }
